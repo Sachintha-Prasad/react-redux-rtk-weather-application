999f8ffdb620a5cb50b6710b78c9e92efc1913be
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-18 00:34:13 +0530

    feat: add weather component with fetched data from redux store

---
+++ src/features/weather/WeatherComponent.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react"
+import { useAppDispatch } from "../../hooks/useAppDispatch"
+import { useAppSelector } from "../../hooks/useAppSelector"
+import { fetchWeather, weatherSelector } from "./weatherSlice"
+import { Input } from "antd"
+
+const WeatherComponent = () => {
+    const { weather, isLoading, error } = useAppSelector(weatherSelector)
+    const dispatch = useAppDispatch()
+    const [searchInput, setSearchInput] = useState("")
+
+    const handleWeatherSearch = () => {
+        if (searchInput) {
+            dispatch(fetchWeather(searchInput))
+        }
+    }
+
+    useEffect(() => {
+        dispatch(fetchWeather("New york"))
+    }, [dispatch])
+
+    console.log(weather)
+
+    return (
+        <div>
+            <Input.Search
+                placeholder="input search text"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.currentTarget.value)}
+                onSearch={handleWeatherSearch}
+                loading={isLoading}
+                allowClear
+                required={true}
+                enterButton
+                size="large"
+            />
+            <div>
+                {error ? (
+                    <div>Error: {error}</div>
+                ) : (
+                    weather && <div>Weather Data: {weather?.name}</div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default WeatherComponent
